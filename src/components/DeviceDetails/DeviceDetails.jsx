067c81dfc46c3d2fd3668cc77444e42f8042f0be
Arif Hossan <arif.hossan.eu@gmail.com>
2023-08-16 01:18:12 +0600

    new portion added

--- src/components/DeviceDetails/DeviceDetails.jsx
+++ src/components/DeviceDetails/DeviceDetails.jsx
@@ -4,6 +4,7 @@ const DeviceDetails = (props) => {
     const {price} =props;
     return (
         <div>
+            <h2>New portion Price</h2>
             <p>Price of this product : {price} Bdt</p>
         </div>
     );
